6fbfa4e69f0e626703564473302633a8f39ef306
Lukáš Goldšmíd <lukas.goldsmid@uhk.cz>
2023-03-28 14:29:28 +0200

    Komit.

--- src/main/java/Hlavni.java
+++ src/main/java/Hlavni.java
@@ -1,3 +1,7 @@
+/**
+ * Komentář v třídě hlavní
+ * Další komentář
+ */
 public class Hlavni {
     public static void main(String[] args) {
         System.out.println("Ahoj"); //revize - zmrazime projekt v čase a sá se jit zpět
