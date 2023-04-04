3e7f56082f36142960326aed7a8405d2a1d53258
Lukáš Goldšmíd <lukas.goldsmid@uhk.cz>
2023-04-04 13:25:09 +0200

    Práce v klonu

--- src/main/java/Hlavni.java
+++ src/main/java/Hlavni.java
@@ -4,6 +4,7 @@
  */
 public class Hlavni {
     public static void main(String[] args) {
-        System.out.println("Ahoj"); //revize - zmrazime projekt v čase a sá se jit zpět
+        System.out.println("Ahoj"); //revize - zmrazime projekt v čase a dá se jit zpět
+        System.out.println("Toto bylo napsáno v klonu.");
     }
 }
